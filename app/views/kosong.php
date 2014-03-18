264337895c533cf0e506a4668b5e408b3f47fd5b
Putu Wiramaswara Widya <wira@wira-HP-ENVY-dv4-Notebook-PC.(none)>
2014-03-19 02:32:07 +0700

    KALIH!

---
+++ app/views/kosong.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<!-- Built with Divshot - http://www.divshot.com -->
+<html>
+  <head>
+    <title>New Page</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js" type="text/javascript"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <div class="container">
+      <center>
+        <h1><strong>SIM</strong>TugasAkhir</h1>
+        <h3>Sistem informasi pengelolaan Tugas Akhir Mahasiswa S1</h3>
+        <h3>Institut Teknologi Sepuluh Nopember</h3>
+        <hr/>
+        <h4><strong>SEDANG PENGERJAAN</strong></h4>
+      </center>
+    </div>
+  </body>
+</html>

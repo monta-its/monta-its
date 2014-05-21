6d70a02f1a0373018936312fe5f500d50526cb1d
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2014-05-21 15:59:12 +0700

    isi_panduan

--- app/views/pages/dasbor/panduan/index.blade.php
+++ app/views/pages/dasbor/panduan/index.blade.php
@@ -5,59 +5,59 @@
 <script type="text/javascript" src="{{URL::to('assets/angular/angular.min.js')}}"></script>
 <script type="text/javascript" src="{{URL::to('assets/angular/angular-route.min.js')}}"></script>
 <script>
-var app = angular.module('dasborisi_panduan', ['ngRoute'], function($interpolateProvider) {
+var app = angular.module('dasborPanduan', ['ngRoute'], function($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
 }).run(function($rootScope, $http) {
-    updateisi_panduan($rootScope, $http);
+    updatePanduan($rootScope, $http);
 });
 
-var updateisi_panduan = function($rootScope, $http) {
-    $http.get('{{URL::to('/dasbor/dosen/isi_panduan')}}').success(function(data) {
+var updatePanduan = function($rootScope, $http) {
+    $http.get('{{URL::to('/dasbor/dosen/panduan')}}').success(function(data) {
         $rootScope.items = data;
     });
 };
 
 
-app.controller('daftarisi_panduanController', function($scope, $http, $rootScope) {
-    $scope.hapusisi_panduan = function(id_isi_panduan) {
-        $http.delete('{{URL::to('/dasbor/dosen/isi_panduan')}}', {'params': {'id_isi_panduan': String(id_isi_panduan)}}).success(function(data) {
-            updateisi_panduan($rootScope, $http);
-            alert('isi_panduan dihapus');
+app.controller('daftarPanduanController', function($scope, $http, $rootScope) {
+    $scope.hapusPanduan = function(id_panduan) {
+        $http.delete('{{URL::to('/dasbor/dosen/panduan')}}', {'params': {'id_panduan': String(id_panduan)}}).success(function(data) {
+            updatePanduan($rootScope, $http);
+            alert('Panduan dihapus');
         });
     };
 });
 
-app.controller('isi_panduanSuntingController', function($rootScope, $scope, $http, $routeParams, $location) {
+app.controller('panduanSuntingController', function($rootScope, $scope, $http, $routeParams, $location) {
     var method = $routeParams.method;
     $scope.method = method;
     if(method == "baru") {
-        $scope.isi_panduan = {};
-        $scope.isi_panduan.judul_isi_panduan = "";
-        $scope.isi_panduan.isi = "";
-        $scope.isi_panduan.lampiran = "";
-        $scope.terbitkanisi_panduan = function() {
-            $http.post('{{{URL::to('/dasbor/dosen/isi_panduan')}}}', $scope.isi_panduan).success(function(data) {
-                updateisi_panduan($rootScope, $http)
+        $scope.panduan = {};
+        $scope.panduan.judul_panduan = "";
+        $scope.panduan.isi_panduan = "";
+        $scope.panduan.lampiran = "";
+        $scope.terbitkanPanduan = function() {
+            $http.post('{{{URL::to('/dasbor/dosen/panduan')}}}', $scope.panduan).success(function(data) {
+                updatePanduan($rootScope, $http)
                 $location.path('/');
             })
         };
     } else if (method == "sunting") {
         if($routeParams.id) {
-            $scope.isi_panduan = {};
-            $scope.isi_panduan.id_isi_panduan = $routeParams.id;
-            $scope.suntingisi_panduan = function() {
-                $http.put('{{URL::to('/dasbor/dosen/isi_panduan')}}', $scope.isi_panduan).success(function (data) {
-                    updateisi_panduan($rootScope, $http);
+            $scope.panduan = {};
+            $scope.panduan.id_panduan = $routeParams.id;
+            $scope.suntingPanduan = function() {
+                $http.put('{{URL::to('/dasbor/dosen/panduan')}}', $scope.panduan).success(function (data) {
+                    updatePanduan($rootScope, $http);
                     $location.path('/');
                 });
             };
             console.log($rootScope.items);
             $.each($rootScope.items, function(i, val) {
-                if(val.id_isi_panduan === $scope.isi_panduan.id_isi_panduan) {
-                    $scope.isi_panduan.judul_isi_panduan = val.judul_isi_panduan;
-                    $scope.isi_panduan.isi = val.isi;
-                    $scope.isi_panduan.lampiran = val.lampiran;
+                if(val.id_panduan === $scope.panduan.id_panduan) {
+                    $scope.panduan.judul_panduan = val.judul_panduan;
+                    $scope.panduan.isi_panduan = val.isi_panduan;
+                    $scope.panduan.lampiran = val.lampiran;
                 }
             });
         } else {
@@ -71,9 +71,9 @@ app.controller('isi_panduanSuntingController', function($rootScope, $scope, $htt
 
 app.config(function($routeProvider) {
     $routeProvider
-    .when('/', { templateUrl: 'daftarisi_panduan.html'})
-    .when('/:method', { templateUrl: 'isi_panduanSunting.html'})
-    .when('/:method/:id', { templateUrl: 'isi_panduanSunting.html'})
+    .when('/', { templateUrl: 'daftarPanduan.html'})
+    .when('/:method', { templateUrl: 'panduanSunting.html'})
+    .when('/:method/:id', { templateUrl: 'panduanSunting.html'})
 });
 
 app.config(function($httpProvider) {
@@ -82,38 +82,38 @@ app.config(function($httpProvider) {
 </script>
 <div class="row">
     <div class="col-md-12">
-        <h1 class="page-header">Kelola isi_panduan</h1>
+        <h1 class="page-header">Kelola Panduan</h1>
     </div>
 </div>
-<div ng-app="dasborisi_panduan">
+<div ng-app="dasborPanduan">
     <ng-view>
     </ng-view>
 
-    <script type="text/ng-template" id="isi_panduanSunting.html">
+    <script type="text/ng-template" id="panduanSunting.html">
 
         <div class="row">
         </div>
-        <form role="form" action="" method="post" accept-charset="utf-8" ng-controller="isi_panduanSuntingController">
+        <form role="form" action="" method="post" accept-charset="utf-8" ng-controller="panduanSuntingController">
             <div class="row">
                 <div class="col-md-8">
                     <div class="form-group">
-                        <input ng-model="isi_panduan.judul_isi_panduan" type="text" class="form-control input-lg" id="judul_isi_panduanisi_panduan" name="judul_isi_panduanisi_panduan" placeholder="judul_isi_panduan isi_panduan">
+                        <input ng-model="panduan.judul_panduan" type="text" class="form-control input-lg" id="judul_panduanPanduan" name="judul_panduanPanduan" placeholder="judul_panduan panduan">
                     </div>
                     <div class="form-group">
-                        <textarea ng-model="isi_panduan.isi" class="form-control" rows="10" name="isiisi_panduan" id="isiisi_panduan"></textarea>
+                        <textarea ng-model="panduan.isi_panduan" class="form-control" rows="10" name="isiPanduan" id="isiPanduan"></textarea>
                     </div>
                     <div class="form-group">
-                        <input placeholder="Masukkan URL/pranala menuju lampiran (dari Dropbox/Google Drive)" ng-model="isi_panduan.lampiran" class="form-control" rows="10" name="lampiranisi_panduan" id="lampiranisi_panduan" />
+                        <input placeholder="Masukkan URL/pranala menuju lampiran (dari Dropbox/Google Drive)" ng-model="panduan.lampiran" class="form-control" rows="10" name="lampiranPanduan" id="lampiranPanduan" />
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <button ng-show="method=='baru'" type="submit" name="aksi" value="Terbitkan" class="btn btn-success" ng-click="terbitkanisi_panduan()">Terbitkan</button>
-                    <button ng-show="method=='sunting'" type="submit" name="aksi" value="Sunting" class="btn btn-success" ng-click="suntingisi_panduan()">Sunting</button>
+                    <button ng-show="method=='baru'" type="submit" name="aksi" value="Terbitkan" class="btn btn-success" ng-click="terbitkanPanduan()">Terbitkan</button>
+                    <button ng-show="method=='sunting'" type="submit" name="aksi" value="Sunting" class="btn btn-success" ng-click="suntingPanduan()">Sunting</button>
                 </div>
             </div>
         </form>
     </script>
-    <script type="text/ng-template" id="daftarisi_panduan.html">
+    <script type="text/ng-template" id="daftarPanduan.html">
         <div class="row" >
             <div class="col-md-6">
                 <a href="#/baru" class="btn btn-default">Buat Baru</a>
@@ -123,13 +123,13 @@ app.config(function($httpProvider) {
                     <input ng-model="searchText" type="text" class="form-control input-xs"  placeholder="Pencarian">
                 </div>
             </div>
-        <div class="row" ng-controller="daftarisi_panduanController">
+        <div class="row" ng-controller="daftarPanduanController">
             <div class="col-md-12">
                 <table class="table">
                     <thead>
                         <tr>
                             <th>No.</th>
-                            <th>judul_isi_panduan</th>
+                            <th>judul_panduan</th>
                             <th>Penulis</th>
                             <th>Waktu</th>
                             <th>Aksi</th>
@@ -137,13 +137,13 @@ app.config(function($httpProvider) {
                     </thead>
                     <tbody>
                         <tr ng-repeat="item in items | filter: searchText | orderBy:'created_at':true">
-                            <td>[[item.id_isi_panduan]]</td>
-                            <td>[[item.judul_isi_panduan]]</td>
+                            <td>[[item.id_panduan]]</td>
+                            <td>[[item.judul_panduan]]</td>
                             <td>[[item.pegawai.nama_lengkap]]</td>
                             <td>[[item.updated_at]]</td>
                             <td>
-                                <a href="#/sunting/[[item.id_isi_panduan]]">Sunting</a>
-                                <a href="#/" ng-click="hapusisi_panduan([[item.id_isi_panduan]])">Hapus</a>
+                                <a href="#/sunting/[[item.id_panduan]]">Sunting</a>
+                                <a href="#/" ng-click="hapusPanduan([[item.id_panduan]])">Hapus</a>
                             </td>
                         </tr>
                     </tbody>

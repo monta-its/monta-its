563e661bb3ff2928dc7ca0212d7cd474cf2a9018
Ifan Iqbal <ifaniqbal.com@gmail.com>
2014-06-08 23:40:18 +0700

    #38: SIt In: add messages

--- app/views/pages/dasbor/sit_in/mahasiswa.blade.php
+++ app/views/pages/dasbor/sit_in/mahasiswa.blade.php
@@ -43,7 +43,7 @@ app.controller('sitInController', function($scope, $http, $rootScope) {
     $scope.buatDataTugasAkhir = function(id_sit_in) {
         if(confirm('Konfirmasikan pemrosesan TA dari Sitin?')) {
             $http.post('{{URL::to('/dasbor/mahasiswa/proses_ta')}}', {id_sit_in: String(id_sit_in)}).success(function(data) {
-                alert('Tugas Akhir telah dibuat.');
+                alert(data.pesan);
                 update($rootScope, $http);
             });
         }
@@ -100,10 +100,10 @@ app.config(function($httpProvider) {
                                 <td ng-show="item.status == -1">Dibatalkan (menunggu)</td>
                                 <td ng-show="item.status == 0">Diajukan</td>
                                 <td ng-show="item.status == 1">Disetujui</td>
-                                <td ng-show="item.status == 2">TA Terproses</td>
+                                <td ng-show="item.status == 2">Bimbingan</td>
                                 <td ng-show="item.status != -1" class="text-center">
                                     <a class="btn btn-xs btn-warning" ng-show="item.status==0" ng-click="batalkanSitIn([[item.id_sit_in]])">Batalkan</a>
-                                    <a class="btn btn-xs btn-success" ng-show="item.status==1" ng-click="buatDataTugasAkhir([[item.id_sit_in]])">Proses TA</a>
+                                    <a class="btn btn-xs btn-success" ng-show="item.status==1" ng-click="buatDataTugasAkhir([[item.id_sit_in]])">Proses Bimbingan</a>
                                 </td>
                             </tr>
                         </tbody>
